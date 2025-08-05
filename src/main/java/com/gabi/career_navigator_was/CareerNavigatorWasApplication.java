716f5836f13bf628014808700f0956c2c6503f97
이유진 프로/솔루션개발팀/AIT Story <yjlee@aitstory.com>
2025-08-05 15:28:58 +0900

    First Commit

---
+++ src/main/java/com/gabi/career_navigator_was/CareerNavigatorWasApplication.java
@@ -0,0 +1,13 @@
+package com.gabi.career_navigator_was;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerNavigatorWasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerNavigatorWasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabi/career_navigator_was/CareerNavigatorWasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabi.career_navigator_was;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerNavigatorWasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
