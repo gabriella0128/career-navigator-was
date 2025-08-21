be41fb616620fedef55dd13974ce7817d5c4bf5c
이유진 프로/솔루션개발팀/AIT Story <yjlee@aitstory.com>
2025-08-21 17:11:32 +0900

    20250821 아이디 정규식 변경

--- src/main/java/com/gabi/career_navigator_was/domain/user/dto/request/IdCheckReq.java
+++ src/main/java/com/gabi/career_navigator_was/domain/user/dto/request/IdCheckReq.java
@@ -6,8 +6,8 @@ import jakarta.validation.constraints.Pattern;
 public record IdCheckReq(
 	@NotBlank
 	@Pattern(
-		regexp = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])[A-Za-z\\d]{8,20}$",
-		message = "사용자 ID는 8~20자의 영문 대소문자와 숫자로만 이루어져야 합니다."
+		regexp = "^[a-z0-9]{8,20}$",
+		message = "사용자 ID는 8~20자의 영문 소문자와 숫자로만 이루어져야 합니다."
 	)
 	String userId
 ) {
